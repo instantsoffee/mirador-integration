26a5ea6b151646744543ab4baa8464d9698619f1
Sophie Hammer <sophie.hammer@onb.ac.at>
2024-02-07 10:05:03 +0100

    get manifestId from url, add test button in plugin

--- src/components/custom_metadata.js
+++ src/components/custom_metadata.js
@@ -3,12 +3,17 @@ import Typography from '@material-ui/core/Typography';
 import { LabelValueMetadata } from 'mirador/dist/es/src/components/LabelValueMetadata';
 
 export default function () {
+  const handleButtonClick = () => {
+    alert('You clicked the button!');
+  };
+
+
   return <>
     <Typography
       variant="h5"
       component="h6"
     >
-      Custom metadata
+      <button onClick={handleButtonClick}>Test button</button>
     </Typography>
     <LabelValueMetadata labelValuePairs={[{ label: 'Cat?', values: ['Yes!'] }]} />
   </>;
